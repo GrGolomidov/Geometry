c9c11fd92140e460c510cac7dd9a75d940675529
Голомидов Григорий Юрьевич <iv822s03@bwp08.csc.local>
2019-05-11 12:54:29 +0700

    source code update

--- main.cpp
+++ main.cpp
@@ -1,17 +1,11 @@
 #include <cstring>
+#include <iostream>
 #include <string>
 #include <vector>
-#include <iostream>
 
 struct pointer {
     double x;
     double y;
-};
-
-struct figure_triangle {
-    pointer vertex_first;
-    pointer vertex_second;
-    pointer vertex_third;
 };
 
 struct figure_circle {
@@ -19,29 +13,27 @@ struct figure_circle {
     double radius;
 };
 
-void skip_space(char* str, std::size_t &i)
+void skip_space(char* str, std::size_t& i)
 {
     while (str[i] == ' ') {
         i++;
     }
 }
 
-void check_name_figure(char* str, std::size_t &i, std::size_t &begin_word, std::size_t &end_word)
+void check_name_figure(
+        char* str,
+        std::size_t& i,
+        std::size_t& begin_word,
+        std::size_t& end_word)
 {
     if (isalpha(str[i])) {
-		begin_word = i;
-	}
+        begin_word = i;
+    }
     while (isalpha(str[i])) {
-		i++;
-	}
-	end_word = i - 1;
+        i++;
+    }
+    end_word = i - 1;
 }
-
-/*bool compare_string_circle(char* str, char* f)
-{
-	int size = 6;
-	for (std::size_t i = 0; i < size)
-}*/
 
 int name_figure(char* str, std::size_t begin_word, std::size_t end_word)
 {
@@ -72,68 +64,80 @@ double square_circle(figure_circle circle, const double PI)
     return (PI * circle.radius * circle.radius);
 }
 
+std::size_t len_str(char* str)
+{
+	std::size_t size = 0;
+	for(size_t i = 0; str[i] != '\0'; i++) {
+		size++;
+	}
+	return size;
+}
 
 int main()
 {
     const double PI = 3.1415926535;
-    //figure_triangle triangle;
+    // figure_triangle triangle;
     figure_circle circle;
 
     char str[100];
     std::cin.getline(str, 100);
+	std::size_t size = len_str(str);
     std::size_t i = 0;
-	std::size_t begin_word, end_word;
+    std::size_t begin_word, end_word;
     skip_space(str, i);
-	check_name_figure(str, i, begin_word, end_word);
-    int figure = name_figure(str, begin_word, end_word); //1 - circle, 2 - triangle
-	skip_space(str, i);
-    if (figure == 1) {
-        if (str[i] == '(') {
-            i++;
-            char *EndPtr;
-            skip_space(str, i);
-            char coordinate_x[10] = {};
-            for (std::size_t j = 0; str[i] == ' '; i++, j++) {
-                coordinate_x[j] = str[i];
-            }
-            circle.vertex.x = std::strtod(coordinate_x, &EndPtr);
-            skip_space(str, i);
-            char coordinate_y[10] = {};
-            for (std::size_t j = 0; str[i] == ' '; i++, j++) {
-                coordinate_y[j] = str[i];
-            }
-            circle.vertex.y = std::strtod(coordinate_y, &EndPtr);
-			i++;
-            if (str[i] == ',') {
-                i++;
-                skip_space(str, i);
-                char radius_circle[] = "";
-                for (std::size_t j = 0; str[i] == ' '; i++, j++) {
-                    radius_circle[j] = str[i];
-                }
-                circle.radius = std::strtod(radius_circle, &EndPtr);
-                skip_space(str, i);
-                if (str[i] == ')') {
-                    i++;
-                    std::cout << "Perimetr circle: " << perimetr_circle(circle, PI) << std::endl;
-                    std::cout << "Square circle: " << square_circle(circle, PI) << std::endl;
-                } else {
-                    std::cout << "missing )" << std::endl;
-                }
-            }
-        } else {
-            std::cout << "missing (" << std::endl;
-        }
-    }/* else if (figure == 2) {
-        if ((str[i] == '(') && (str[i + 1] == '(')) {
-            i = i + 2;
-            skip_space(str, i);
-        } else {
-            std::cout << "missing ((" << std::endl;
-        }
-    }*/
-	else {
+    check_name_figure(str, i, begin_word, end_word);
+    int figure = name_figure(
+            str, begin_word, end_word); // 1 - circle
+    skip_space(str, i);
+    if (figure != 1) {
 		std::cout << "Error name figure" << std::endl;
+		return 0;
 	}
+    if (str[i] != '(') {
+        std::cout << "missing (" << std::endl;
+        return 0;
+    }
+    i++;
+    char* EndPtr;
+    skip_space(str, i);
+    char coordinate_x[100] = {};
+    for (std::size_t j = 0; str[i] != ' '; i++, j++) {
+        coordinate_x[j] = str[i];
+    }
+    circle.vertex.x = std::strtod(coordinate_x, &EndPtr);
+    skip_space(str, i);
+    char coordinate_y[100] = {};
+    for (std::size_t j = 0; ((str[i] != ' ') && (str[i] != ',')); i++, j++) {
+        coordinate_y[j] = str[i];
+    }
+//circle(0 0, 1.5)
+    circle.vertex.y = std::strtod(coordinate_y, &EndPtr);
+	skip_space(str, i);
+    if (str[i] != ',') {
+        std::cout << "missing ," << std::endl;
+        return 0;
+    }
+    i++;
+    skip_space(str, i);
+    char radius_circle[100] = {};
+    for (std::size_t j = 0; ((str[i] != ' ') && (str[i] != ')')); i++, j++) {
+        radius_circle[j] = str[i];
+    }
+    circle.radius = std::strtod(radius_circle, &EndPtr);
+    skip_space(str, i);
+    if (str[i] != ')') {
+        std::cout << "missing )" << std::endl;
+        return 0;
+    }
+    i++;
+  	skip_space(str, i);
+    if (i != size) {
+    	std::cout << "There are extraneous characters after )" << std::endl;
+		return 0;
+	}
+    std::cout << "Perimetr circle: "
+              << perimetr_circle(circle, PI) << std::endl;
+    std::cout << "Square circle: " << square_circle(circle, PI)
+              << std::endl;
     return 0;
 }
